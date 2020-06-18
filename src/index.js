435a20ad2ecfe4165d1f5db37cf06c906aff044b
phpusr <phpusr@gmail.com>
2020-06-18 20:49:38 +0500

    Added player control

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {app, BrowserWindow} from 'electron'
+import {createPlayer} from './player.js'
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -15,26 +16,15 @@ const createWindow = () => {
 
     mainWindow.loadURL('https://music.yandex.ru')
     // mainWindow.loadURL(`file://${__dirname}/index.html`)
-    createPlayer()
 
-    // mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools()
     mainWindow.webContents.on('did-finish-load', () => {
-        const code = `
-            setInterval(() => {
-                const title = document.querySelector('.d-link.deco-link.track__title').innerText
-                console.log('title', title)
-            }, 1000)
-        `
-        mainWindow.webContents.executeJavaScript(code)
+        createPlayer(mainWindow)
     })
 
     mainWindow.on('closed', () => {
         mainWindow = null
     })
-}
-
-function createPlayer() {
-    require('./player.js')
 }
 
 app.on('ready', createWindow)
--- src/player.js
+++ src/player.js
@@ -1,50 +1,76 @@
-const Player = require('mpris-service')
+export function createPlayer(mainWindow) {
+	const Player = require('mpris-service')
 
-const player = Player({
-	name: 'YandexMusic',
-	identity: 'Node.js media player',
-	supportedUriSchemes: ['file'],
-	supportedMimeTypes: ['audio/mpeg', 'application/ogg'],
-	supportedInterfaces: ['player']
-})
-
-player.getPosition = function() {
-  // return the position of your player
-  return 0
-}
-
-// Events
-const events = ['raise', 'quit', 'next', 'previous', 'pause', 'playpause', 'stop', 'play', 'seek', 'position', 'open', 'volume', 'loopStatus', 'shuffle']
-events.forEach(function (eventName) {
-	player.on(eventName, function () {
-		console.log('Event:', eventName, arguments)
+	const player = Player({
+		name: 'YandexMusic',
+		identity: 'YandexMusic media player',
+		supportedUriSchemes: ['file'],
+		supportedMimeTypes: ['audio/mpeg', 'application/ogg'],
+		supportedInterfaces: ['player']
 	})
-})
 
-player.on('quit', function () {
-	process.exit()
-})
-
-setTimeout(function () {
-	// @see http://www.freedesktop.org/wiki/Specifications/mpris-spec/metadata/
-	player.metadata = {
-		'mpris:trackid': player.objectPath('track/0'),
-		'mpris:length': 60 * 1000 * 1000, // In microseconds
-		'mpris:artUrl': 'http://www.adele.tv/images/facebook/adele.jpg',
-		'xesam:title': 'Lolol',
-		'xesam:album': '21',
-		'xesam:artist': ['Adele']
+	player.getPosition = function() {
+	  // return the position of your player
+	  return 0
 	}
 
-	player.playbackStatus = Player.PLAYBACK_STATUS_PLAYING
+	// language=JavaScript
+	const scripts = `
+		class YandexMusicPlayer {
+			playPause() {
+				console.log('>> playPause')
+				let playPauseButtonDom = document.querySelector('.player-controls__btn_play')
+				if (!playPauseButtonDom) {
+					playPauseButtonDom = document.querySelector('.player-controls__btn_pause')
+				}
+				
+				playPauseButtonDom.click()
+			}
+			next() {
+			    console.log('>> next')
+			    document.querySelector('.player-controls__btn_next').click()
+			}
+			prev() {
+			    console.log('>> prev')
+				document.querySelector('.player-controls__btn_prev').click()
+			}
+		}
+		const app = new YandexMusicPlayer()
+	`
+	mainWindow.webContents.executeJavaScript(scripts)
 
-	console.log('Now playing: Lolol - Adele - 21')
+	// Events
+	player.on('playpause', () => {
+		mainWindow.webContents.executeJavaScript('app.playPause()')
+	})
 
+	player.on('next', () => {
+		mainWindow.webContents.executeJavaScript('app.next()')
+	})
 
-}, 1000)
+	player.on('previous', () => {
+		mainWindow.webContents.executeJavaScript('app.prev()')
+	})
 
-setTimeout(() => {
-  player.seeked(0)
-}, 2000)
+	player.on('quit', function () {
+		process.exit()
+	})
 
-export default player
+	setTimeout(function () {
+		// @see http://www.freedesktop.org/wiki/Specifications/mpris-spec/metadata/
+		player.metadata = {
+			'mpris:trackid': player.objectPath('track/0'),
+			'mpris:length': 60 * 1000 * 1000, // In microseconds
+			'mpris:artUrl': 'http://www.adele.tv/images/facebook/adele.jpg',
+			'xesam:title': 'Lolol',
+			'xesam:album': '21',
+			'xesam:artist': ['Adele']
+		}
+
+		player.playbackStatus = Player.PLAYBACK_STATUS_PLAYING
+	}, 1000)
+
+	setTimeout(() => {
+	  player.seeked(0)
+	}, 2000)
+}
