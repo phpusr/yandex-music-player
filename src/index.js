d76d31af000d18a696fcd8bbf1f4a12ba959a98d
phpusr <phpusr@gmail.com>
2020-06-22 14:33:35 +0500

    Removed electron-squirrel-startup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 const {app, BrowserWindow, Menu, BrowserView} = require('electron')
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-    app.quit()
-}
-
 let mainWindow
 let webContents
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
