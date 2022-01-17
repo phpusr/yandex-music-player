c8587dfcff644ce67334b4aa764d36949819b1b7
phpusr <phpusr@gmail.com>
2022-01-17 13:19:54 +0500

    Removed fixing auto pause

--- src/preload.js
+++ src/preload.js
@@ -94,13 +94,6 @@ class YandexMusicPlayer {
         adDom.style.display = 'none'
       }
     })
-
-    // Fixing auto pause with ad
-    const button = q('.crackdown-popup__close')
-    if (button && !button.parentNode.classList.contains('popup_hidden')) {
-      button.click()
-      q(PLAY_BUTTON).click()
-    }
   }
 }
 
